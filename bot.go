4b0843338f9b2115dae69c301f9cf40f56f4eea7
Miroslav Tynovsky <tynovsky@avast.com>
2017-08-29 17:48:23 +0200

    version 0.00001

---
+++ bot.go
@@ -0,0 +1,14 @@
+//
+// bot.go
+// Copyright (C) 2017 tynovsky <tynovsky@tyrdeb>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world, I'm bot")
+}
