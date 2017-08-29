f6a0836a41f71a01fcda3232248197b6002d7cc6
Miroslav Tynovsky <tynovsky@avast.com>
2017-08-29 18:06:25 +0200

    turn off debug, filter out some message types

--- bot.go
+++ bot.go
@@ -18,34 +18,22 @@ func main() {
 	api := slack.New(os.Getenv("GOSSIPBOT_TOKEN"))
 	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
 	slack.SetLogger(logger)
-	api.SetDebug(true)
+	//api.SetDebug(true)
 
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
-		fmt.Print("Event Received: ")
 		switch ev := msg.Data.(type) {
-		case *slack.HelloEvent:
-			// Ignore hello
-
-		case *slack.ConnectedEvent:
-			fmt.Println("Infos:", ev.Info)
-			fmt.Println("Connection counter:", ev.ConnectionCount)
-			// Replace #general with your Channel ID
-			rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", "#general"))
 
 		case *slack.MessageEvent:
-			fmt.Printf("Message: %v\n", ev)
-
-		case *slack.PresenceChangeEvent:
-			fmt.Printf("Presence Change: %v\n", ev)
+			fmt.Printf("Message: %+v\n", ev)
 
 		case *slack.LatencyReport:
-			fmt.Printf("Current latency: %v\n", ev.Value)
+			//fmt.Printf("Current latency: %v\n", ev.Value)
 
 		case *slack.RTMError:
-			fmt.Printf("Error: %s\n", ev.Error())
+			//fmt.Printf("Error: %s\n", ev.Error())
 
 		case *slack.InvalidAuthEvent:
 			fmt.Printf("Invalid credentials")
