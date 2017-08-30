087e1fdc691e1a8e430331f320f105edb259a430
Miroslav Tynovsky <tynovsky@avast.com>
2017-08-30 22:55:30 +0200

    refactor main function

--- bot.go
+++ bot.go
@@ -27,6 +27,8 @@ func (message mesg) isMessageImportant() bool {
 	return message.emojiCount > 1
 }
 
+var messages map[string]mesg
+
 func main() {
 	api := slack.New(os.Getenv("GOSSIPBOT_TOKEN"))
 	log.SetLevel(log.DebugLevel)
@@ -40,7 +42,7 @@ func main() {
 	}
 	archivesRootURL := fmt.Sprintf("https://%s.slack.com/archives", info.Domain)
 
-	messages := make(map[string]mesg, 0)
+	messages = make(map[string]mesg)
 
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
@@ -49,66 +51,16 @@ func main() {
 			log.Debug("Connection counter:", ev.ConnectionCount)
 
 		case *slack.MessageEvent:
-			if ev.SubType == "" {
-				if ev.ThreadTimestamp != "" {
-					var m = messages[ev.ThreadTimestamp]
-					m.replyCount = m.replyCount + 1
-					messages[ev.ThreadTimestamp] = m
-				} else {
-					channel, err := api.GetChannelInfo(ev.Channel)
-					if err != nil {
-						log.Fatal(err)
-					}
-					user, err := api.GetUserInfo(ev.User)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					messages[ev.Timestamp] = mesg{
-						messageText:  ev.Text,
-						replyCount:   0,
-						emojiCount:   0,
-						channelID:    ev.Channel,
-						channelName:  channel.Name,
-						userID:       ev.User,
-						username:     user.Name,
-						userRealname: user.RealName,
-						timestamp:    ev.Timestamp,
-					}
-				}
-
-				log.Debug("== Processing new message event: \n")
-				log.Debug("message object: %+v\n", ev)
-				log.Debug("Message: %s\n", ev.Text)
-				log.Debug("Subtype: %s\n", ev.SubType)
-				log.Debug("Timestamp: %s\n", ev.Timestamp)
-				log.Debug("ThreadTimestamp: %s\n", ev.ThreadTimestamp)
-				log.Debug("Messages: %+v\n", messages)
-			}
+			processMessageEvent(api, ev)
 
 		case *slack.ReactionAddedEvent:
-			log.Debug("-------------\n")
-			log.Debug("Reaction: %+v\n", ev)
-
-			var m = messages[ev.Item.Timestamp]
-			if ev.Item.Timestamp != "" {
-				m.emojiCount = m.emojiCount + 1
-				messages[ev.Item.Timestamp] = m
-			}
-
-			if m.isMessageImportant() {
+			processReactionAddedEvent(api, ev)
+			if m := messages[ev.EventTimestamp]; m.isMessageImportant() {
 				forwardMessage(m, rtm, archivesRootURL)
 			}
 
 		case *slack.ReactionRemovedEvent:
-			log.Debug("-------------\n")
-			log.Debug("Reaction removed: %+v\n", ev)
-
-			if ev.Item.Timestamp != "" {
-				var m = messages[ev.Item.Timestamp]
-				m.emojiCount = m.emojiCount - 1
-				messages[ev.Item.Timestamp] = m
-			}
+			processReactionRemovedEvent(api, ev)
 
 		case *slack.InvalidAuthEvent:
 			log.Error("Invalid credentials")
@@ -117,6 +69,68 @@ func main() {
 		default:
 		}
 	}
+}
+
+func processMessageEvent(api *slack.Client, ev *slack.MessageEvent) {
+	if ev.SubType == "" {
+		if ev.ThreadTimestamp != "" {
+			var m = messages[ev.ThreadTimestamp]
+			m.replyCount = m.replyCount + 1
+			messages[ev.ThreadTimestamp] = m
+		} else {
+			channel, err := api.GetChannelInfo(ev.Channel)
+			if err != nil {
+				log.Fatal(err)
+			}
+			user, err := api.GetUserInfo(ev.User)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			messages[ev.Timestamp] = mesg{
+				messageText:  ev.Text,
+				replyCount:   0,
+				emojiCount:   0,
+				channelID:    ev.Channel,
+				channelName:  channel.Name,
+				userID:       ev.User,
+				username:     user.Name,
+				userRealname: user.RealName,
+				timestamp:    ev.Timestamp,
+			}
+		}
+
+		log.Debug("== Processing new message event: \n")
+		log.Debug("message object: %+v\n", ev)
+		log.Debug("Message: %s\n", ev.Text)
+		log.Debug("Subtype: %s\n", ev.SubType)
+		log.Debug("Timestamp: %s\n", ev.Timestamp)
+		log.Debug("ThreadTimestamp: %s\n", ev.ThreadTimestamp)
+		log.Debug("Messages: %+v\n", messages)
+	}
+}
+
+func processReactionAddedEvent(api *slack.Client, ev *slack.ReactionAddedEvent) {
+	log.Debug("-------------\n")
+	log.Debug("Reaction: %+v\n", ev)
+
+	var m = messages[ev.Item.Timestamp]
+	if ev.Item.Timestamp != "" {
+		m.emojiCount = m.emojiCount + 1
+		messages[ev.Item.Timestamp] = m
+	}
+}
+
+func processReactionRemovedEvent(api *slack.Client, ev *slack.ReactionRemovedEvent) {
+	log.Debug("-------------\n")
+	log.Debug("Reaction removed: %+v\n", ev)
+
+	if ev.Item.Timestamp != "" {
+		var m = messages[ev.Item.Timestamp]
+		m.emojiCount = m.emojiCount - 1
+		messages[ev.Item.Timestamp] = m
+	}
+
 }
 
 func forwardMessage(message mesg, rtm *slack.RTM, archivesURL string) {
