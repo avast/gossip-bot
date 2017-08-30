7b3fa291e8dd2fa74cd9ee54bafc2eb6840eef15
Ondřej Hošák <ondrej.hosak@avg.com>
2017-08-30 11:21:50 +0200

    WIP: forwarding messages

--- bot.go
+++ bot.go
@@ -10,6 +10,7 @@ import (
 
 type mesg struct {
 	text       string
+	channel 	 string
 	emojiCount int
 	replyCount int
 	timestamp  string
@@ -19,7 +20,6 @@ func main() {
 	api := slack.New(os.Getenv("GOSSIPBOT_TOKEN"))
 	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
 	slack.SetLogger(logger)
-	//api.SetDebug(true)
 
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
@@ -30,13 +30,11 @@ func main() {
 		switch ev := msg.Data.(type) {
 
 		case *slack.ConnectedEvent:
-			//fmt.Println("Infos:", ev.Info)
 			fmt.Println("Connection counter:", ev.ConnectionCount)
 
 		case *slack.MessageEvent:
 			if ev.SubType == "" {
 				if ev.ThreadTimestamp != "" {
-					// messages[ev.ThreadTimestamp].count++
 					var m = messages[ev.ThreadTimestamp]
 					m.replyCount = m.replyCount + 1
 					messages[ev.ThreadTimestamp] = m
@@ -45,11 +43,13 @@ func main() {
 						text:       ev.Text,
 						replyCount: 0,
 						emojiCount: 0,
+						channel:			ev.Channel,
 						timestamp:  ev.Timestamp,
 					}
 				}
 
 				fmt.Printf("-------------\n")
+				fmt.Printf("message object: %+v\n", ev)
 				fmt.Printf("Message: %s\n", ev.Text)
 				fmt.Printf("Subtype: %s\n", ev.SubType)
 				fmt.Printf("Timestamp: %s\n", ev.Timestamp)
@@ -67,6 +67,8 @@ func main() {
 				messages[ev.Item.Timestamp] = m
 			}
 
+			evaluateMessage(messages[ev.Item.Timestamp], rtm)
+
 		case *slack.InvalidAuthEvent:
 			fmt.Printf("Invalid credentials")
 			return
@@ -75,4 +77,14 @@ func main() {
 			//			fmt.Printf(".")
 		}
 	}
+}
+
+func evaluateMessage(message mesg, rtm *slack.RTM) {
+	if message.emojiCount > 1 {
+		message_to_forward := rtm.NewOutgoingMessage(
+			fmt.Sprintf("There is interesting message in: %s %s", message.text, message.channel),
+			os.Getenv("GOSSIPBOT_CHANNEL"))
+
+		rtm.SendMessage(message_to_forward)
+  }
 }
